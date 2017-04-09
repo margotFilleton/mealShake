91020ad93445f7afad5dae6ce1290c25a746ca8c
margotfilleton <margot.filleton@gmail.com>
2017-04-09 19:44:49 +0200

    API key file

---
+++ mealShake/APIKey.swift
@@ -0,0 +1,13 @@
+//
+//  ApiKeyManager.swift
+//  mealShake
+//
+//  Created by Margot Filleton on 09/04/2017.
+//  Copyright © 2017 Margot Filleton. All rights reserved.
+//
+
+import Foundation
+
+public class APIkey {
+    static let googleMapsAPIKey = "YOUR_API_KEY"
+}
