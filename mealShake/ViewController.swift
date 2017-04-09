260765e362b4a76abc47e00d943bbf4f0087f95c
margotfilleton <margot.filleton@gmail.com>
2017-04-09 18:40:23 +0200

    init project

---
+++ mealShake/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mealShake
+//
+//  Created by Margot Filleton on 09/04/2017.
+//  Copyright © 2017 Margot Filleton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
