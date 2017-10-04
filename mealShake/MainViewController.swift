76cbe089e6f3d2acdc5b50fb0b5cc6e41352c55b
margotfilleton <margot.filleton@gmail.com>
2017-10-05 01:07:34 +0200

    Fix bug : "Need shake twice before get data location"

--- mealShake/MainViewController.swift
+++ mealShake/MainViewController.swift
@@ -16,14 +16,16 @@ class MainViewController: UIViewController {
     private let cardWidth: CGFloat = 280
     private let cardHeight: CGFloat = 468
     
-
+    override func viewWillAppear(_ animated: Bool) {
+        GoogleMapsManager.instance.getUserLocation()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = Theme.mainColor
         let shakeView = UIImageView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
         shakeView.contentMode = .scaleAspectFit
         shakeView.image = shake
-        view.addSubview(shakeView)       
+        view.addSubview(shakeView)
     }
     
     //Device Shaken
